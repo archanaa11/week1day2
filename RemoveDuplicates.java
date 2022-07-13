c7324e8478faac7ccc32ae809b9862540c6b7927
archanaa11 <38030135+archanaa11@users.noreply.github.com>
2022-07-13 09:59:18 +0530

    Add files via upload

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -5,7 +5,7 @@ public class RemoveDuplicates {
 	public static void main(String[] args) {
 		/*
 		 * Pseudo code 
-		 
+
 		 * a) Use the declared String text as input
 			String text = "We learn java basics as part of java sessions in java week1";		
 		 * b) Initialize an integer variable as count	  
@@ -13,40 +13,37 @@ public class RemoveDuplicates {
 		 * d) Initialize another loop to check whether the word is there in the array
 		 * e) if it is available then increase and count by 1. 
 		 * f) if the count > 1 then replace the word as "" 
-		 
+
 		 * g) Displaying the String without duplicate words	
 		 */
-		
+
 		String text = "We learn java basics as part of java sessions in java week1";
 		int count=0;
 		String r="java";
 		String[] s=text.split(" ");
-		for(int i=0;i<s.length;i++) 
+				
+		for(int j=0;j<s.length;j++)
 		{
-			if(r.equals(s[i]))
+			if(r.equals(s[j]))
 			{
-				count++;}
-			}
-		
-		if(count>1)
-		{
-			for(int j=0;j<s.length;j++)
-			{
-				if(r.equals(s[j]))
+				count++;
+				if(count==1) {
+				System.out.print(s[j]+" ");
+				}
+				else if(count>1)
 				{
 					s[j]="";
-				System.out.print(s[j]);	
-				}
-				else
-				{
-					System.out.print(s[j]+ " ");
+					System.out.print(s[j]);	
 				}
 			}
-				
+			else
+			{
+				System.out.print(s[j]+ " ");
+			}
 		}
-		
-		
 
-	}
+	}	
 
 }
+
+
